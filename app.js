a9cdd7bed4155c53d3b71240dcb621cd77d8b22c
makoge boris <makogeboris@gmail.com>
2024-07-03 15:50:21 +0100

    modified

--- app.js
+++ app.js
@@ -1,13 +1,107 @@
 "use strict";
 
-// fetch("https://api.github.com/users/{username}")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching the user:", error);
-//   });
+const errMsg = document.querySelector(".form__error-msg");
+const btn = document.querySelector(".form__btn");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".form");
+  const input = document.querySelector(".form__input");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = input.value;
+    if (username) {
+      getUser(username);
+    }
+  });
+
+  getUser("octocat");
+});
+
+const renderUser = function (data) {
+  const avatar = document.querySelector(".user__avatar");
+  const displayName = document.querySelector(".user__name");
+  const userName = document.querySelector(".user__username");
+  const dateJoined = document.querySelector(".user__date-joined");
+  const bio = document.querySelector(".user__bio");
+  const repos = document.querySelector(".user__stats-repo");
+  const followers = document.querySelector(".user__stats-followers");
+  const following = document.querySelector(".user__stats-following");
+  const userLocation = document.querySelector(".user__location");
+  const userWebsite = document.querySelector(".user__info-website");
+  const twitterUrl = document.querySelector(".user__info-twitter");
+  const company = document.querySelector(".user__info-company");
+
+  avatar.src = data.avatar_url;
+
+  displayName.textContent = data.name || data.login;
+  userName.textContent = `@${data.login}`;
+  userName.href = data.html_url;
+
+  const date = new Date(data.created_at);
+  const day = date.getUTCDate();
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.getUTCFullYear();
+  dateJoined.textContent = `Joined ${day} ${month} ${year}`;
+
+  bio.textContent = data.bio || "This profile has no bio";
+  bio.style.opacity = data.bio ? 1 : 0.5;
+
+  repos.textContent = data.public_repos;
+  followers.textContent = data.followers;
+  following.textContent = data.following;
+
+  userLocation.textContent = data.location || "Not Available";
+  userLocation.style.opacity = data.location ? 1 : 0.5;
+
+  if (data.blog) {
+    userWebsite.textContent = data.blog;
+    userWebsite.href = data.blog;
+    userWebsite.style.opacity = 1;
+  } else {
+    userWebsite.textContent = "Not Available";
+    userWebsite.style.opacity = 0.5;
+    userWebsite.removeAttribute("href");
+  }
+
+  if (data.twitter_username) {
+    twitterUrl.textContent = data.twitter_username;
+    twitterUrl.href = `https://x.com/${data.twitter_username}`;
+    twitterUrl.style.opacity = 1;
+  } else {
+    twitterUrl.textContent = "Not Available";
+    twitterUrl.style.opacity = 0.5;
+    twitterUrl.removeAttribute("href");
+  }
+
+  if (data.company) {
+    company.textContent = data.company;
+    const companyLink = data.company.slice(1);
+    company.href = `https://github.com/${companyLink}`;
+    company.style.opacity = 1;
+  } else {
+    company.textContent = "Not Available";
+    company.style.opacity = 0.5;
+    company.removeAttribute("href");
+  }
+};
+
+const getUser = async function (username) {
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    if (!res.ok) {
+      errMsg.style.display = "block";
+      throw new Error("No results");
+    }
+
+    errMsg.style.display = "none";
+
+    const data = await res.json();
+    renderUser(data);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
 const themeBtn = document.querySelector(".header__theme-btn");
 
